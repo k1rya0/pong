b03ca257f9af543006aeb1ae749cc5c2ae78935f
kirill st. <staroshchukkirill@gmail.com>
2024-06-04 18:02:38 +0200

    clean unity projects

---
+++ Assets/movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        print("hey from start");
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody2D.velocity = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody2D.velocity = Vector2.down;
+        }
+        else
+        {
+           rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.green;
+
+    }
+}
